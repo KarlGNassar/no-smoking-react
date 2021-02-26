5ce6010a943ec87e1eb4db671badb9bbe78d073b
KARL NASSAR <kgnassar01@gmail.com>
2021-02-26 19:21:08 +0200

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDe4Jr8QPeE9NDyfxsrHJWwHRkfVQc9JR8",
     authDomain: "no-smoking-react.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
     appId: "1:110980884703:web:6aae8f72008515728c616a",
     measurementId: "G-N1W0TWP4PZ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebaseApp.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { db, auth, provider} 
